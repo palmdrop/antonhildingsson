09dcac1679a6f1a15c0444a5d186eebf35dd13a4
palmdrop <anton@exlex.se>
2024-07-03 22:13:44 +0200

    fix: use custom channel download instead of arena-js

--- scripts/download-arena-sprawl.js
+++ scripts/download-arena-sprawl.js
@@ -1,12 +1,14 @@
 import fs from 'fs/promises';
-import Arena from "are.na";
+// import Arena from "are.na";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env.local" });
 
+/*
 const arena = new Arena({
   accessToken: process.env.ARENA_ACCESS_TOKEN
 });
+*/
 
 const slug = "stream-cascade-sprawl";
 
@@ -60,18 +62,75 @@ const processBlock = async (block, published) => {
   }
 }
 
+export const downloadChannel = async (
+  channel,
+  pageSize = 100,
+  accessToken = process.env.ARENA_ACCESS_TOKEN
+) => {
+  console.log(`Done fetching channel "${channel}"`);
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${accessToken}`
+  }
+
+  try {
+    const thumbResponse = await fetch(
+      `https://api.are.na/v2/channels/${channel}/thumb`,
+      {
+        method: "GET",
+        headers
+      }
+    );
+
+    const thumbJson = await thumbResponse.json();
+
+    const length = thumbJson.length;
+    const numberOfPages = Math.ceil(length / pageSize);
+
+    const fetchPage = async pageNumber => {
+      console.log(`Fetching page ${pageNumber}...`);
+
+      const response = await fetch(
+        `https://api.are.na/v2/channels/${channel}/contents?page=${pageNumber}&per=${pageSize}`,
+        {
+          method: "GET",
+          headers
+        }
+      );
+
+      return response.json();
+    }
+
+    const pages = await Promise.all(
+      [...Array(numberOfPages).keys()].map(fetchPage)
+    );
+
+    console.log(`Done fetching channel "${channel}"`);
+    console.log(pages)
+    return pages;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 console.log("Downloading Sprawl...");
 
+/*
 arena
   .channel(slug)
   .get()
   .then(async channel => {
-    const blocks = channel.contents;
+  */
+downloadChannel(slug).then(async pages => {
+    // const blocks = channel.contents;
+    const blocks = pages.flatMap(page => page.contents);
 
     console.log("Processing blocks...");
 
     await fs.mkdir("./src/content/sprawl/external", { recursive: true });
-    await Promise.all(blocks.map(block => processBlock(block, channel.published)));
+    // await Promise.all(blocks.map(block => processBlock(block, channel.published)));
+    await Promise.all(blocks.map(block => processBlock(block, false)));
 
     console.log("Done processing blocks!");
   })
