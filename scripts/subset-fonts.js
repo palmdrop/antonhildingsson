2e3453d20221cb2804e3d0ffdab6cf79354e0406
palmdrop <anton@exlex.se>
2024-08-05 13:49:51 +0200

    chore: remove unused fonts

--- scripts/subset-fonts.js
+++ scripts/subset-fonts.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 import glob from "fast-glob";
 
 const files = glob.sync("build/**/*.html");
-const command = `npx subfont ${files.join(' ')} -i --root build --inline-css`;
+const command = `npx subfont ${files.join(' ')} -i --root build --no-fallbacks`;
 
 exec(command, (error, stdout, stderr) => {
   if (error || stderr) {
