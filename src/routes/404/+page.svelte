e58b09a97d478a3856b110a566786a9c806c1ac5
palmdrop <anton@exlex.se>
2024-08-13 09:36:28 +0200

    feat: add 404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import TitleText from "$lib/components/text/TitleText.svelte";
+</script>
+
+<section class="main-grid full-width">
+  <TitleText>
+    hittar inte hit.
+  </TitleText>
+  <p class="full-width">
+    Du gick vilse. Denna sida existerar inte.
+  </p>
+  <p class="full-width">
+    <a href="/" class="clickable">
+      Tillbaka till stigen.
+    </a>
+  </p>
+</section>
